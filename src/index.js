8c6668f2cb92e57bc6d7d0565e15c1a7880b95f8
Derek Chatham <Derek.ChathamSr@outlook.com>
2021-12-28 11:43:58 -0600

    Adding What I had from previous verison

--- src/index.js
+++ src/index.js
@@ -1,8 +1,112 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { callApi } from './api';
+import { AccountForm, Posts, SinglePost, NewPostForm} from './components';
+
+
+
+
+
 
 const App = () => {
-  return <div>NO STOP</div>;
+  const [token, setToken] = useState('');
+  const [userData, setUserData] = useState({});
+  const [posts, setPosts] = useState([]);
+
+  const fetchUserData = async (token) => {
+    const { data } = await callApi({
+      url: '/users/me',
+      token,
+    });
+    return data;
+  };
+
+  const fetchPosts = async () => {
+    const {
+      data: { posts },
+    } = await callApi({
+      url: '/posts',
+    });
+    return posts;
+  };
+
+  useEffect(async () => {
+    // const posts = await fetchPosts();
+    // setPosts(posts);
+    if (!token) {
+      setToken(localStorage.getItem('token'));
+      return;
+    }
+    const data = await fetchUserData(token);
+    if (data && data.username) {
+      setUserData(data);
+    }
+  }, [token]);
+
+  useEffect(async () => {
+    const posts = await fetchPosts();
+    setPosts(posts);
+  }, []);
+
+  return (
+    <>
+      <h1>Stranger's Things</h1>
+      <span>
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/posts/new">Add A Post</Link>
+      <Link to="/posts">See All Post</Link>
+       </span>
+    
+      <Switch>
+        <Route exact path="/">
+          {userData.username && <div>Hello {userData.username}</div>}
+        </Route>
+        <Route exact path="/posts">
+          <Posts posts={posts} />
+        </Route>
+        <Route path="/posts/new">
+          <NewPostForm
+            token={token}
+            setPosts={setPosts}
+            posts={posts}
+            action="add"
+          />
+        </Route>
+        <Route path="/posts/:postId/edit">
+          <NewPostForm
+            token={token}
+            setPosts={setPosts}
+            posts={posts}
+            action="edit"
+          />
+        </Route>
+        <Route path="/posts/:postId">
+          <SinglePost posts={posts} />
+        </Route>
+        <Route path="/register">
+          <AccountForm
+            action="register"
+            setToken={setToken}
+            setUserData={setUserData}
+          />
+        </Route>
+        <Route path="/login">
+          <AccountForm
+            action="login"
+            setToken={setToken}
+            setUserData={setUserData}
+          />
+        </Route>
+      </Switch>
+    </>
+  );
 };
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('app')
+);
