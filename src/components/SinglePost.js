b5c015a6f37135353b9af731f318fba60ee3e220
Derek Chatham <Derek.ChathamSr@outlook.com>
2022-01-01 18:16:50 -0600

    new stuff

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -2,10 +2,6 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 
-import React from 'react';
-import { useParams } from 'react-router-dom';
-
-
 const SinglePost = ({ posts }) => {
   const { postId } = useParams();
   const post = posts.find((post) => postId === post._id);
@@ -47,7 +43,5 @@ const SinglePost = ({ posts }) => {
     </>
   );
 };
-
-export default SinglePost;
 
 export default SinglePost;
